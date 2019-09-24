e75ed797700ec92e3ade834063e76aad7a72df24
kaxterzz <kasunsmbox@hotmail.com>
2019-09-24 13:30:38 +0530

    initial commit

---
+++ configs/keys.js
@@ -0,0 +1,14 @@
+const KEYS = {
+    'googleOauth': {
+        'clientID': "926547240840-22ol6miopsovtvslqrcsq1mc86ve3bj5.apps.googleusercontent.com",
+        'clientSecret': "hwIwiWbSS__iFZ8bygkjJykj",
+        'callback': '/auth/google/callback'
+    },
+    "session_key": "secret123"
+}
+
+module.exports = KEYS
+
+
+
+
